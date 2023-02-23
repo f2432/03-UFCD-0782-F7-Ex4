f074ed82099464be7a8c19942a49d8daf63d3f68
f2432 <109317080+f2432@users.noreply.github.com>
2023-02-23 14:40:49 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ echo "A executar os testes ..."
 echo
 
 saida=$(echo -e "5\n6\n7\n7\n8\n7\n" | ./a.out)
-esperado="Salário semanal: 445 Euros"
+esperado="Salário semanal: 175 Euros"
 
 if [ "$saida" != "$esperado" ]; then
     echo "Teste falhou: a saída do programa é diferente do esperado"
